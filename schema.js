ece91c089ae35f4005067661f6fc705009615372
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-07-04 18:05:28 +0400

    added editCustomers, and wen now have a complete CRUD GraphQL server

--- schema.js
+++ schema.js
@@ -86,7 +86,20 @@ const mutation = new GraphQLObjectType({
 				return axios.delete('http://localhost:3000/customers/' + args.id)
 					.then(res => res.data);
 			}
-		}
+		},
+		editCustomer: {
+			type: CustomerType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) },
+				name: {type: GraphQLString },
+				email: { type: GraphQLString },
+				age: { type: GraphQLInt },
+			},
+			resolve: (parentValue, args) => {
+				return axios.patch('http://localhost:3000/customers/' + args.id, args)
+					.then(res => res.data);
+			}
+		},
 	}
 });
 
