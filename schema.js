61de9896df2c28d920ea048717d786f65c8e5ed9
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-07-04 17:54:58 +0400

    added deleteCustomer mutation

--- schema.js
+++ schema.js
@@ -57,6 +57,41 @@ const RootQuery = new GraphQLObjectType({
 	}
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addCustomer: {
+			type: CustomerType,
+			args: {
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				email: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) }
+			},
+			resolve: (parentValue, args) => {
+				return axios.post('http://localhost:3000/customers', {
+					name: args.name,
+					email: args.email,
+					age: args.age
+				})
+					.then(res => res.data);
+			}
+		},
+		deleteCustomer: {
+			type: CustomerType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) }
+			},
+			resolve: (parentValue, args) => {
+				return axios.delete('http://localhost:3000/customers/' + args.id)
+					.then(res => res.data);
+			}
+		}
+	}
+});
+
+
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation
 });
