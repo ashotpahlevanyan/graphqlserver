b007d1b2856ebfa2180cf2b20810ff589e6bb7b6
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-01-09 11:29:39 +0400

    added opening the url and one command start

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const opn  = require('opn');
 
 const schema = require('./schema');
 
@@ -12,4 +13,5 @@ app.use('/graphql', expressGraphQL({
 
 app.listen(4000, () => {
 	console.log('Server running on port 4000');
+	opn('http://localhost:4000/graphql');
 });
